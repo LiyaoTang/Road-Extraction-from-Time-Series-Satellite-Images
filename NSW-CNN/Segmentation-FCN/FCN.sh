9ac600204e29bc17131a51c36b0424e910ba016a
beihaifusang <beihaifusang@gmail.com>
2018-04-19 18:18:53 +1000

    concat operation fixed

--- NSW-CNN/Segmentation-FCN/FCN.sh
+++ NSW-CNN/Segmentation-FCN/FCN.sh
@@ -8,19 +8,19 @@ epoch=20
 POS=0
 for CONV in "16-32-64-128"; do
     for OUT_CONV in "3"; do
-        for catIn in "0" "1-16|1-32"; do
+        for catIn in "0" "1-16;1-32"; do
             for NORM in m G ; do
                 for RAND in 0; do
 
                     # weighted
+                    echo ${name}
                     name=FCN_${CONV}_${OUT_CONV}_cat${catIn}_weight_${NORM}_p${POS}_e${epoch}_r${RAND}
                     python FCN.py --conv ${CONV} --output_conv ${OUT_CONV} --concat_input ${catIn} --norm ${NORM} --pos ${POS} --save ${save_dir} --record_summary --rand ${RAND} --gpu ${RAND} --epoch ${epoch} >./Log/FCN/${name}  2>&1
-                    echo ${name}
 
                     # weighted, batch norm
+                    echo ${name}
                     name=FCN_${CONV}_${OUT_CONV}_cat${catIn}_weight_bn_${NORM}_p${POS}_e${epoch}_r${RAND}
                     python FCN.py --conv ${CONV} --output_conv ${OUT_CONV} --concat_input ${catIn} --norm ${NORM} --pos ${POS} --save ${save_dir} --record_summary --rand ${RAND} --gpu ${RAND} --epoch ${epoch} --use_batch_norm >./Log/FCN/${name}  2>&1
-                    echo ${name}
 
                     sleep 10m
                     job_cnt=$((job_cnt+2))
